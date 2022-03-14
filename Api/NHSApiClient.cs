f90aac89df84acc266e3ac241ccbd5d9d186c530
Aiden <47725379+Mewrin@users.noreply.github.com>
2022-03-14 21:12:19 +0000

    move duplicate code into one HttpGetAsync method
    Moving duplicate code for deserialization of data from the NHS API GET response into a method that is used for all current and future client methods

--- Api/NHSApiClient.cs
+++ Api/NHSApiClient.cs
@@ -11,35 +11,53 @@ namespace Assignment2022_NCC.Api
     public class NHSApiClient : INHSApiClient
     {
         private const string CommonHealthQuestionsApiBaseUrl = "https://api.nhs.uk/common-health-questions";
+        private const string RatingsAndReviewsApiBaseUrl = "https://api.nhs.uk/comments";
 
         private HttpClient _httpClient;
 
         public NHSApiClient()
         {
             _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("subscription-key", Settings.ApiKey);
         }
 
         public CommonHealthQuestionsApiResonse? GetCommonHealthQuestions()
         {
-            var commonHealthQuestionsApiResponse = new CommonHealthQuestionsApiResonse();
-            _httpClient.DefaultRequestHeaders.Add("subscription-key", Settings.ApiKey);
-
             try
             {
-                var rawResponse = _httpClient.GetAsync(CommonHealthQuestionsApiBaseUrl)
-                    .ContinueWith((rawResponse) =>
-                    {
-                        var asyncString = rawResponse.Result.Content.ReadAsStringAsync();
-                        asyncString.Wait();
-                        commonHealthQuestionsApiResponse = JsonConvert.DeserializeObject<CommonHealthQuestionsApiResonse>(asyncString.Result);
-                    });
-                rawResponse.Wait();
-                return commonHealthQuestionsApiResponse;
+                return HttpGetAsync<CommonHealthQuestionsApiResonse>(CommonHealthQuestionsApiBaseUrl);
             }
             catch(Exception)
             {
                 return null;
             }
+        }
+
+        public RatingsAndReviewsApiResponse? GetRatingsAndReviews(RatingsAndReviewsApiRequest request)
+        {
+            try
+            {
+                return HttpGetAsync<RatingsAndReviewsApiResponse>(RatingsAndReviewsApiBaseUrl);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private T HttpGetAsync<T>(string url) where T : new()
+        {
+            T response = new T();
+            var rawResponse = _httpClient.GetAsync(url)
+                    .ContinueWith((rawResponse) =>
+                    {
+                        var asyncString = rawResponse.Result.Content.ReadAsStringAsync();
+                        asyncString.Wait();
+                        response = JsonConvert.DeserializeObject<T>(asyncString.Result);
+                    });
+
+            rawResponse.Wait();
+            return response;
         }
     }
 }
---
+++ Api/Types/RatingsAndReviewsApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2022_NCC.Api.Types
+{
+    public class RatingsAndReviewsApiRequest
+    {
+    }
+
+    public class RatingsAndReviewsApiResponse
+    {
+    }
+}
