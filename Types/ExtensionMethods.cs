462b4da4761f32c4e174b8eb3388057af581d194
Aiden <47725379+Mewrin@users.noreply.github.com>
2022-03-14 22:58:05 +0000

    add extension methods
    Added extension methods 'IsNullOrEmpty' for strings and lists to easily check if the object has a value that can be used - also added a warning message when no comments can be found for an organisation when looking up by odsCode

---
+++ Types/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿public static class ExtensionMethods
+{
+    public static bool IsNullOrEmpty(this string value)
+    {
+        return value == null || value.Length == 0;
+    }
+
+    public static bool IsNullOrEmpty<T>(this List<T> value)
+    {
+        return value == null || value.Count() == 0;
+    }
+}
