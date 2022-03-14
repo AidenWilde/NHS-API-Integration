51fc421a995fb2635165ccd3cb0a66f16f0c9ac4
Aiden <47725379+Mewrin@users.noreply.github.com>
2022-03-14 21:01:24 +0000

    Added a new page for new NHS Api Endpoint
    added a new razor web page for the Ratings and Reviews NHS Api endpoint 'https://developer.api.nhs.uk/nhs-api/documentation/comments' so that the information and interactions are separate from the common health questions API integration page

---
+++ Pages/RatingsAndReviews.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment2022_NCC.Pages
+{
+    public class RatingsAndReviewsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
